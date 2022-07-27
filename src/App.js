15804a84c743e285aef9269fa38b65cb4ba7ee8a
avolkers <alexandervolkers@gmail.com>
2022-07-27 15:02:39 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={"flex flex-col justify-between flex-1 min-h-screen w-full overflow-hidden"} data-theme={"cyberpunk"}>
+            <div className={"container mx-auto"}>
+                <h1 className={"text-accent"}>APP</h1>
+                <div className="btn btn-primary">LOL</div>   
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+﻿import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyByYk_vX4qRROXNeSnANH-nwHmyQjoeDcY",
+    authDomain: "fir-house-market.firebaseapp.com",
+    projectId: "fir-house-market",
+    storageBucket: "fir-house-market.appspot.com",
+    messagingSenderId: "190686078631",
+    appId: "1:190686078631:web:58cd63ca0c28b15cd1dd82"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
