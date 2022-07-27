13465b13f08e3f75093c424243f3f7b101a55fe9
avolkers <alexandervolkers@gmail.com>
2022-07-27 17:06:09 +0200

    Created navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import Navbar from "./Components/Navbar";
 
 function App() {
     const [curTheme, setCurTheme] = useState('cyberpunk');
@@ -8,24 +9,14 @@ function App() {
         if (theme) {
             setCurTheme(theme);
         }
-    }, []);
+    }, []);   
     
-    const randomTheme = () => {
-        const themes = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"];
-        const randomIndex = Math.floor(Math.random() * themes.length);
-        
-        let randomTheme = themes[randomIndex]        
-        setCurTheme(randomTheme);
-        localStorage.setItem('theme', randomTheme);
-    }
-    
-    return (
+    return (        
         <div className={"flex flex-col justify-between flex-1 min-h-screen w-full overflow-hidden"} data-theme={curTheme}>
+            <Navbar setTheme={setCurTheme} />
+            
             <div className={"container mx-auto"}>
-                <div className={"flex flex-row justify-between"}>
-                    <h1 className={"text-accent text-3xl font-semibold"}>MY APP</h1>
-                    <div onClick={randomTheme} className="btn btn-primary ml-8">Randomize Theme</div>
-                </div>                
+                
             </div>
         </div>
     );
