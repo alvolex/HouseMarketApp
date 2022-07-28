72683b4a226c6e45837074a2d33d8e7b9a3e9d93
avolkers <alexandervolkers@gmail.com>
2022-07-28 23:22:02 +0200

    Some minor tweaks to the signup pages

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -5,19 +5,18 @@ function SignIn() {
     const navigate = useNavigate();
     
     return (
-        <div className={"container mx-auto flex-1 flex-col flex justify-center"}> 
-            
-            <div className="card shadow-2xl bg-base-100 w-4/5 md:w-1/2 mx-auto border-2 border-secondary">
-                <h1 className={"mx-auto text-6xl pb-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent"}>Login</h1>
+        <div className={"container mx-auto flex-1 flex-col flex justify-center"}>             
+            <div className="card shadow-md shadow-secondary bg-base-300 w-4/5 md:w-1/2 mx-auto border-2 border-secondary">
+                <h1 className={"mx-auto text-6xl pb-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent mt-6"}>LOGIN</h1>
                 <div className="card-body px-6 pb-6 pt-0">
                     <div className="form-control">
-                        <label className="label">
+                        <label className="label font-bold">
                             <span className="label-text bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">Email</span>
                         </label>
                         <input type="text" placeholder="E-mail" className="input input-bordered"/>
                     </div>
                     <div className="form-control">
-                        <label className="label">
+                        <label className="label font-bold">
                             <span className="label-text bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">Password</span>
                         </label>
                         <input type="password" placeholder="Password" className="input input-bordered"/>
@@ -33,7 +32,7 @@ function SignIn() {
                 
             </div>
 
-            <h1 className={"mx-auto text-2xl py-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent text-center"}>Don't have an account? <p onClick={()=>{navigate("/sign-up")}} className={"origin-center -rotate-[8deg] italic btn btn-secondary hover:cursor-pointer relative bottom-1  ml-4"}>Signup!</p></h1>
+            <h1 className={"mx-auto text-2xl py-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent text-center"}>Don't have an account? <p onClick={()=>{navigate("/sign-up")}} className={"origin-center -rotate-[8deg] hover:rotate-[0deg] italic btn btn-secondary hover:cursor-pointer relative bottom-1  ml-4 shadow-sm shadow-primary hover:shadow-none"}>Signup!</p></h1>
         </div>
     );
 }
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -7,23 +7,23 @@ function SignUp() {
     return (
         <div className={"container mx-auto flex-1 flex-col flex justify-center"}>
 
-            <div className="card shadow-2xl bg-base-100 w-4/5 md:w-1/2 mx-auto border-2 border-secondary">
-                <h1 className={"mx-auto text-6xl pb-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent"}>Sign up</h1>
+            <div className="card shadow-2xl bg-base-300 w-4/5 md:w-1/2 mx-auto border-2 border-secondary">
+                <h1 className={"mx-auto text-6xl pb-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent "}>SIGN UP</h1>
                 <div className="card-body px-6 pb-6 pt-0">
                     <div className="form-control">
-                        <label className="label">
+                        <label className="label font-bold">
                             <span className="label-text bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">Email</span>
                         </label>
                         <input type="text" placeholder="E-mail" className="input input-bordered"/>
                     </div>
                     <div className="form-control">
-                        <label className="label">
+                        <label className="label font-bold">
                             <span className="label-text bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">Password</span>
                         </label>
                         <input type="password" placeholder="Password" className="input input-bordered"/>
                     </div>
                     <div className="form-control">
-                        <label className="label">
+                        <label className="label font-bold">
                             <span className="label-text bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">Repeat Password</span>
                         </label>
                         <input type="password" placeholder="Password" className="input input-bordered"/>
@@ -36,7 +36,7 @@ function SignUp() {
 
             </div>
 
-            <h1 className={"mx-auto text-2xl py-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent text-center"}>Have an account? <p onClick={()=>{navigate("/sign-in")}} className={"origin-center -rotate-[8deg] italic btn btn-secondary hover:cursor-pointer relative bottom-1 ml-4"}>Login!</p></h1>
+            <h1 className={"mx-auto text-2xl py-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent text-center"}>Have an account? <p onClick={()=>{navigate("/sign-in")}} className={"origin-center -rotate-[8deg] hover:rotate-[0deg] italic btn btn-secondary hover:cursor-pointer relative bottom-1 ml-4 shadow-sm shadow-primary hover:shadow-none"}>Login!</p></h1>
         </div>
     );
 }
