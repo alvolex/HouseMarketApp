096c017822837a2a37220d39e4e1b0f233a04fdb
avolkers <alexandervolkers@gmail.com>
2022-07-28 17:34:39 +0200

    Even more navbar tweaks

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,17 +11,21 @@ function Navbar() {
     }
     
     return (
-        <div className={"/*xl:container */ sm:max-w-[65%] justify-evenly flex shadow-2xl fixed bottom-0 mx-auto inset-x-0 bg-base-100 rounded-xl"}>            
-            <div onClick={()=>{navigate("/")}} className={`${pathMatchRoute('/') ? 'text-primary bg-base-300' : 'hover:text-secondary opacity-60 hover:opacity-100 hover:bg-base-200'} p-2 flex flex-col align-middle my-2 hover:cursor-pointer  rounded-xl group items-center`}>
+        <div className={"/*xl:container */ sm:max-w-[65%] flex justify-around shadow-2xl fixed bottom-0 mx-auto inset-x-0 bg-base-100 rounded-xl"}>            
+           {/*EXPLORE*/}
+            <div onClick={()=>{navigate("/")}} className={`${pathMatchRoute('/') ? 'text-primary bg-base-300' : 'hover:text-secondary opacity-60  hover:opacity-100 hover:bg-base-200'} w-full p-2 flex flex-col align-middle my-2 hover:cursor-pointer  rounded-xl group items-center mx-2`}>
                 <MdOutlineExplore className={"mx-2 text-6xl"}/>
-                <span className={"text-2xl text-center group-hover:underline decoration-dashed"}>Explore</span>
+                <span className={"text-2xl text-center group-hover:underline decoration-dashed"}>Explore</span>                
             </div>
-            <div onClick={()=>{navigate("/offers")}} className={` ${pathMatchRoute('/offers') ? 'text-primary bg-base-300' : 'hover:text-secondary opacity-60 hover:opacity-100 hover:bg-base-200'} p-2 flex flex-col align-middle my-2 hover:cursor-pointer  rounded-xl group items-center`}>
+    
+            {/*OFFERS*/}
+            <div onClick={()=>{navigate("/offers")}} className={` ${pathMatchRoute('/offers') ? 'text-primary bg-base-300' : 'hover:text-secondary opacity-60 hover:opacity-100 hover:bg-base-200'} w-full p-2 flex flex-col align-middle my-2 hover:cursor-pointer  rounded-xl group items-center`}>
                 <MdOutlineLocalOffer className={"mx-2 text-6xl"}/>
                 <span className={"text-2xl text-center group-hover:underline decoration-dashed"}>Offers</span>
             </div>
             
-            <div onClick={()=>{navigate("/profile")}} className={`${pathMatchRoute('/profile') ? 'text-primary bg-base-300' : 'hover:text-secondary opacity-60 hover:opacity-100 hover:bg-base-200'} p-2 flex flex-col align-middle my-2 hover:cursor-pointer  rounded-xl group items-center`}>
+            {/*PROFILE*/}
+            <div onClick={()=>{navigate("/profile")}} className={`${pathMatchRoute('/profile') ? 'text-primary bg-base-300' : 'hover:text-secondary opacity-60 hover:opacity-100 hover:bg-base-200'} w-full p-2 flex flex-col align-middle my-2 hover:cursor-pointer  rounded-xl group items-center mx-2`}>
                 <MdPersonPin className={"mx-2 text-6xl "}/>
                 <span className={"text-2xl text-center group-hover:underline decoration-dashed"}>Profile</span>
             </div>
